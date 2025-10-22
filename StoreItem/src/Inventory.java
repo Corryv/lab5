d6b854c3dfd155530866f7c36940a599d89a24d9
Shane Gilchrist <gillyssoccer01@gmail.com>
2025-10-22 10:11:09 -0400

    Shane added to main file

--- StoreItem/src/Inventory.java
+++ StoreItem/src/Inventory.java
@@ -12,6 +12,12 @@ public class Inventory implements InventoryInterface{
     private static ArrayList<StoreItem> cartInventory = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
 
+    // Creation of Food Items
+    ShelfStable spam = new ShelfStable("Canned Pork", "SPAM", 2.98, 20, "10/24/2027", false, "12/20/2030");
+    Fruit apple = new Fruit("Granny Smith Apple", "Good & Gather", 1.01, 50, "11/05/2025", true, "Green");
+    Outerwear coat = new Outerwear("Parka", "Calvin Klein", 100.56, 4, "M", "Black", "Nylon", "Long");
+
+
     // method templates to use for interface
 
     public void displayInventory(){
